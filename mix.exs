0ce6d184f420654aeaf8e60a8619e7d0fbbbed2e
Matteo Brancaleoni <mbrancaleoni@voismart.it>
2019-07-09 16:41:56 +0200

    enforce elixir version

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule Conform.MixProject do
     [
       app: :conform,
       version: "2.5.2",
-      elixir: "~> 1.3",
+      elixir: "~> 1.9",
       escript: [main_module: Conform, path: "priv/bin/conform", embed_elixir: embed_elixir?],
       compilers: compilers(@do_peg_compile?),
       build_embedded: Mix.env() == :prod,
